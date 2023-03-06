8b4c1c922bd06991d89d9c727a9f55d722209ad2
Roel Bruggink <roel@jaroel.nl>
2023-03-06 18:08:35 +0100

    scale images a bit better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import {defineConfig} from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
+import image from '@astrojs/image'
 
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), image()],
 })
